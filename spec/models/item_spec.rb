0656f9ca5c28d0941f4e82a0a1c2056898c5feac
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-11 15:45:55 -0600

    Test: Item relationship and validations tested at model level

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do 
+    it { should belong_to :todo }
+  end
+  describe 'validations' do 
+    it { should validate_presence_of :name }
+  end
 end

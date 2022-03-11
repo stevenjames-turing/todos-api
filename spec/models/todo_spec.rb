55d354a07cbc226e489e3370fca09bb6659feacf
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-11 15:45:40 -0600

    Test: Todo relationship and validations tested at model level

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do 
+    it { should have_many :items, dependent: :destroy }
+  end
+  describe 'validations' do 
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :created_by }
+  end
 end

3b05c8b3d87469ac791a67e2f52d681983a69fa2
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-14 22:34:48 -0500

    Test: User validations and relationships tested at model level

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'relationships' do 
+    it { should have_many :todos }
+  end
+  describe 'validations' do 
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password_digest }
+  end
+
+
+end
